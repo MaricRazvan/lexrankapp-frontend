be9fef48ed4c97f099bcb92792d4eda9d6f6eb70
MaricRazvan <maricrazvan@gmail.com>
2025-06-04 16:50:18 +0300

    Updated the post link

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
     setSummary('');
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/summarize', {
+      const response = await fetch('https://lexrankapp-backend.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
